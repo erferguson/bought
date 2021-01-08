1e75a96d56963b4a4348b1ff13a9f7728005677d
Eric Ferguson <ferguson.eric.r@gmail.com>
2021-01-08 13:07:41 -0800

    Navbar > deeper layout and strucuture

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
-import classes from '*.module.css';
+
+import logo from '../../assets/bought-basket.png'
 
 export const Navbar = () => {
     return (
@@ -9,11 +10,18 @@ export const Navbar = () => {
             <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
                     <Typography variant='h6' className={classes.title} color='inherit' >
-                        <img src={} alt='Bought' height='25px' className={classes.image}  />
+                        <img src={logo} alt='Bought' height='25px' className={classes.image}  />
                         Bought
                     </Typography>
+                    <div className={classes.grow} />
+                    <dive className={classes.button}>
+                        <IconButton aria-label='Show cart items' color='inherit'  >
+                            <Badge badgeContent={2} color='secondary'>
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </dive>
                 </Toolbar>
-
             </AppBar>
         </>
     )
